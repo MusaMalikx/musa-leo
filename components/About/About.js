62cebd51d00d643e7e34bbff764bd48c2ec3368f
MusaMalikx <muhammadmusamalik2@gmail.com>
2022-12-28 00:01:51 +0500

    few changes added in about page

--- components/About/About.js
+++ components/About/About.js
@@ -19,14 +19,20 @@ const About = () => {
         >
           About
         </p> */}
-        <div class="md:max-w-xl max-w-xs sm:max-w-lg transition-all ease-in-out mx-auto space-y-6">
-          <div class="flex flex-row flex-nowrap items-center my-8">
+        <div className="md:max-w-xl max-w-xs sm:max-w-lg transition-all ease-in-out mx-auto space-y-6">
+          <div
+            data-aos="zoom-in"
+            className="flex flex-row flex-nowrap items-center my-8"
+          >
             <span
-              class="flex-grow block border-t h-1 dark:border-primary-dark border-primary-light"
+              className="flex-grow block border-t h-1 dark:border-primary-dark border-primary-light"
               aria-hidden="true"
               role="presentation"
             ></span>
-            <span class="flex-none block mx-4 border-2 border-dotted dark:border-primary-light border-primary-dark rounded-md text-xl md:text-4xl px-8 py-3 font-family-dancing-script">
+            <span
+              data-aos="zoom-in"
+              className="flex-none block mx-4 border-2 border-dotted dark:border-primary-light border-primary-dark rounded-md text-xl md:text-4xl px-8 py-3 font-family-dancing-script"
+            >
               About
             </span>
             <span
