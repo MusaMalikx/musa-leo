56ce00db05c2ef74ba1b6c030561c9c62909f754
MusaMalikx <muhammadmusamalik2@gmail.com>
2022-12-29 19:27:10 +0500

    bugs fixed

--- components/Navbar/Navbar.js
+++ components/Navbar/Navbar.js
@@ -17,7 +17,7 @@ const Navbar = () => {
   return (
     <div className="sticky top-0 backdrop-blur-md shadow-md bg-light/40 dark:bg-dark/40 z-10">
       <motion.div
-        className="progress-bar absolute top-[89px] sm:top-[98px] left-0 right-0 h-0.5 bg-primary-light dark:bg-primary-dark rounded-r-lg z-10 origin-left"
+        className="progress-bar absolute top-[88px] sm:top-[98px] left-0 right-0 h-0.5 bg-primary-light dark:bg-primary-dark rounded-r-lg z-10 origin-left"
         style={{ scaleX }}
       />
       <div className="flex items-center justify-between py-7 ">
@@ -36,7 +36,10 @@ const Navbar = () => {
               </DivMotion>
             )}
             {!name && (
-              <DivMotion className=" font-family-montserrat" onMouseLeave={toggleName}>
+              <DivMotion
+                className="font-family-montserrat whitespace-nowrap"
+                onMouseLeave={toggleName}
+              >
                 <strong className="text-primary-light dark:text-primary-dark">M</strong>
                 usa <strong className="text-primary-light dark:text-primary-dark">M</strong>
                 alik
